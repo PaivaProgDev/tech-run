9a7bd062c484812351f657ea77ff1cda865e5662
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-06-08 20:55:32 -0300

    Alterações no design da foto do perfil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import Logo from './Global/Logo'
-import { useAuth } from '../contexts/AuthContext'
-import { Link } from 'react-router-dom'
-import { ArrowRightIcon, UserIcon } from 'lucide-react'
+import React from "react";
+import Logo from "./Global/Logo";
+import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
+import { ArrowRightIcon, UserIcon } from "lucide-react";
 
 const Header = () => {
-    const { userCredential } = useAuth()
+  const { userCredential } = useAuth();
 
-    return (
-        <header className='flex items-center justify-between z-10 bg-white py-3 px-4 border-b fixed top-0 w-full border-gray-300'>
-            <div className='flex items-center gap-3'>
-                {
-                    userCredential.photoURL !== null ?
-                        <div className='rounded-full w-full max-w-[40px] flex items-center justify-center'>
-                            <img className='rounded-full' src={userCredential.photoURL} alt="Foto de perfil do usuário" />
-                        </div>
-                        :
-                        <div className='bg-slate-300 p-1 rounded-full'>
-                            <UserIcon className='size-6 text-[var(--color-2)]' />
-                        </div>
-                }
-                <div>
-                    <pre className='text-[14px] text-[var(--color-3)]'>Olá, <strong>{userCredential.displayName}</strong></pre>
-                    <Link to={'/profile'} className='text-[13px] font-medium text-[var(--color-2)] flex items-center gap-1'>
-                        Ver perfil
-                        <ArrowRightIcon className='size-4 text-[var(--color-2)]' />
-                    </Link>
+  return (
+    <header className="flex items-center justify-between z-10 bg-white py-3 px-4 border-b fixed top-0 w-full border-gray-300">
+      <div className="flex items-center gap-3">
+        {userCredential.photoURL !== null ? (
+          <div className="rounded-full">
+            <img
+              className="rounded-full w-10 h-10 object-cover"
+              src={userCredential.photoURL}
+              alt="Foto de perfil do usuário"
+            />
+          </div>
+        ) : (
+          <div className="bg-slate-300 p-1 rounded-full">
+            <UserIcon className="size-6 text-[var(--color-2)]" />
+          </div>
+        )}
+        <div>
+          <pre className="text-[14px] text-[var(--color-3)]">
+            Olá, <strong>{userCredential.displayName}</strong>
+          </pre>
+          <Link
+            to={"/profile"}
+            className="text-[13px] font-medium text-[var(--color-2)] flex items-center gap-1"
+          >
+            Ver perfil
+            <ArrowRightIcon className="size-4 text-[var(--color-2)]" />
+          </Link>
+        </div>
+      </div>
+      <Logo />
+    </header>
+  );
+};
 
-                </div>
-            </div>
-            <Logo />
-        </header>
-    )
-}
-
-export default Header
+export default Header;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,95 +1,104 @@
-import { ArrowLeftIcon, LogOutIcon, UserIcon } from 'lucide-react';
+import { ArrowLeftIcon, LogOutIcon, UserIcon } from "lucide-react";
 
-import { Navigate, useNavigate } from 'react-router-dom'
-import { signOut, updateProfile } from 'firebase/auth';
-import { auth } from '../services/firebase';
-import { useAuth } from '../contexts/AuthContext'
-import { useEffect, useState } from 'react';
-import { FileUploaderRegular } from '@uploadcare/react-uploader';
-import '@uploadcare/react-uploader/core.css';
-
-
+import { Navigate, useNavigate } from "react-router-dom";
+import { signOut, updateProfile } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { useAuth } from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import { FileUploaderRegular } from "@uploadcare/react-uploader";
+import "@uploadcare/react-uploader/core.css";
 
 const Profile = () => {
-    const navigate = useNavigate()
-    const { setIsAuthenticated, setUserCredential, userCredential } = useAuth()
-    const [photoUrl, setPhotoUrl] = useState(null)
+  const navigate = useNavigate();
+  const { setIsAuthenticated, setUserCredential, userCredential } = useAuth();
 
-    const handleBackPage = () => {
-        navigate(-1)
+  const handleBackPage = () => {
+    navigate(-1);
+  };
+
+  const handleLogOut = () => {
+    signOut(auth);
+    navigate("/login");
+    setIsAuthenticated(false);
+    setUserCredential(null);
+  };
+
+  const handleUpdatePhoto = async (cdnUrl) => {
+    if (auth.currentUser) {
+      updateProfile(auth.currentUser, {
+        photoURL: cdnUrl,
+      });
+
+      setUserCredential({
+        ...auth.currentUser,
+        photoURL: cdnUrl,
+      });
     }
+  };
 
-    const handleLogOut = () => {
-        signOut(auth);
-        <Navigate to={'/login'} />
-        setIsAuthenticated(false)
-        setUserCredential(null)
-    }
+  return (
+    <section className="flex flex-col items-center min-h-screen py-22 px-4 bg-[var(--color-bg)]">
+      <div className="flex w-full items-center justify-between ">
+        <div
+          onClick={handleBackPage}
+          className="cursor-pointer bg-gray-200 w-fit p-2 rounded-full"
+        >
+          <ArrowLeftIcon className="text-[var(--color-2)] size-4.5" />
+        </div>
+        <div
+          onClick={handleLogOut}
+          className="cursor-pointer bg-gray-200 w-fit p-2 rounded-full"
+        >
+          <LogOutIcon className="size-4.5 text-[var(--color-2)]" />
+        </div>
+      </div>
 
+      <div className="mt-10 flex flex-col items-center">
+        {userCredential.photoURL !== null ? (
+          <div className=" relative">
+            <img
+              className="rounded-full h-40 w-40 object-cover"
+              src={userCredential.photoURL}
+              alt="Foto de perfil do usuário"
+            />
+            <FileUploaderRegular
+              sourceList="local, camera"
+              cameraModes="photo"
+              pubkey="56aedfc665a0dce4a57b"
+              className="absolute right-0 bottom-0"
+              onCommonUploadSuccess={(e) =>
+                e.successEntries.map((entry) => {
+                  handleUpdatePhoto(entry.cdnUrl);
+                })
+              }
+            />
+          </div>
+        ) : (
+          <div className="bg-slate-300 p-6 rounded-full relative">
+            <UserIcon className="size-12 text-[var(--color-2)]" />
+            <FileUploaderRegular
+              sourceList="local, camera"
+              cameraModes="photo"
+              pubkey="56aedfc665a0dce4a57b"
+              className="absolute right-0 bottom-0"
+              onCommonUploadSuccess={(e) =>
+                e.successEntries.map((entry) => {
+                  setPhotoUrl(entry.cdnUrl);
+                  handleUpdatePhoto(entry.cdnUrl);
+                })
+              }
+            />
+          </div>
+        )}
+        <h1 className="text-2xl font-semibold text-[var(--color-3)] mt-5 mb-1">
+          <pre>{userCredential.displayName}</pre>
+        </h1>
+        <pre className="text-gray-400 text-center text-[14px]">
+          {userCredential.email}
+        </pre>
+      </div>
+    </section>
+  );
+};
 
-    const user = auth.currentUser
-    console.log(userCredential)
-
-    const handleUpdatePhoto = async (cdnUrl) => {
-        if (user) {
-            updateProfile(user, {
-                photoURL: cdnUrl
-            })
-
-            setUserCredential({
-                ...userCredential,
-                photoURL: cdnUrl
-            })
-        }
-    }
-
-    return (
-        <section className='flex flex-col items-center min-h-screen py-22 px-4 bg-[var(--color-bg)]'>
-            <div className='flex w-full items-center justify-between '>
-                <div onClick={handleBackPage} className='cursor-pointer bg-gray-200 w-fit p-2 rounded-full'>
-                    <ArrowLeftIcon className='text-[var(--color-2)] size-4.5' />
-                </div>
-                <div onClick={handleLogOut} className='cursor-pointer bg-gray-200 w-fit p-2 rounded-full'>
-                    <LogOutIcon className='size-4.5 text-[var(--color-2)]' />
-                </div>
-            </div>
-
-
-            <div className='mt-10 flex flex-col items-center'>
-                {
-                    userCredential.photoURL !== null ?
-                        <div className='rounded-full w-full relative max-w-[8rem] flex items-center justify-center'>
-                            <img className='rounded-full' src={userCredential.photoURL} alt="Foto de perfil do usuário" />
-                            <FileUploaderRegular
-                                sourceList="local, camera"
-                                cameraModes="photo"
-                                pubkey="56aedfc665a0dce4a57b"
-                                className='absolute right-0 bottom-0'
-                                onCommonUploadSuccess={(e) =>
-                                    e.successEntries.map((entry) => {
-                                        setPhotoUrl(entry.cdnUrl)
-                                        handleUpdatePhoto(entry.cdnUrl)
-                                    })
-                                }
-                            />
-                        </div>
-                        :
-                        <div className='bg-slate-300 p-3 rounded-full'>
-                            <UserIcon className='size-12 text-[var(--color-2)]' />
-                        </div>
-                }
-                <h1 className='text-2xl font-semibold text-[var(--color-3)] mt-5 mb-1'>
-                    <pre>{userCredential.displayName}</pre>
-                </h1>
-                <pre className='text-gray-400 text-center text-[14px]'>
-                    {userCredential.email}
-                </pre>
-            </div>
-        </section>
-    )
-
-}
-
-
-
-export default Profile
+export default Profile;
